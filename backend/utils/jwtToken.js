9b136b46a6feecfb0132def39512d0b202bce220
Ayush Raj <ayushraj@Ayushs-MacBook-Air.local>
2026-06-05 18:47:41 +0530

    fix: allow cross-site cookies by setting sameSite to None

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,8 +7,8 @@ export const generateToken = (user, message, statusCode, res) => {
     res.status(statusCode).cookie(cookieName, token, {
       expires: new Date(Date.now() + expireDays * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV === "production",
+      sameSite: "None",
+      secure: true,
     }).json({
       success: true,
       message,
